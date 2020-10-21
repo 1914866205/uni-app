89e343b3b382fbe1242bb1af9d132bf05c8b3224
ntt <1914866205@qq.com>
2020-10-21 09:50:09 +0800

    网盘项目初始化，引入全局样式，自定义图标库，配置单底部导航栏

--- network-dist/pages/index/index.vue
+++ network-dist/pages/index/index.vue
@@ -1,38 +1,29 @@
 <template>
 	<view>
 		<!-- 自定义导航栏 -->
-		<view>
-			<!-- 驼峰式自动转换为中划线式 -->
-			<!-- 顶部 -->
-			<uni-status-bar>
-				<!-- 此处不为rpx -->
-				<view style="height: 44px;" class="flex border-bottom align-center">
-					<view class="flex-1 flex">
-						<!-- ml  margin left 32 -->
-						<text class="font-md ml-3">首页</text>
-					</view>
-					<view class="flex-1 flex justify-center"></view>
-					<view class="flex-1 flex justify-end">
-						<view style="width: 60rpx;height: 60rpx;" class="flex align-center justify-center bg-light rounded-circle mr-3">
-							<text class="iconfont icon-zengjia"></text>
-						</view>
-						<view style="width: 60rpx;height: 60rpx;" class="flex align-center justify-center bg-light rounded-circle mr-3">
-							<text class="iconfont icon-gengduo"></text>
-						</view>
-					</view>
+		<nav-bar>
+			<text slot="left" class="font-md ml-3">首页</text>
+			<template slot="right">
+				<view style="width: 60rpx;height: 60rpx;" class="flex align-center justify-center bg-icon rounded-circle mr-3">
+					<text class="iconfont icon-zengjia"></text>
 				</view>
-			</uni-status-bar>
-		</view>
+				<view style="width: 60rpx;height: 60rpx;" class="flex align-center justify-center bg-icon rounded-circle mr-3">
+					<text class="iconfont icon-gengduo"></text>
+				</view>
+			</template>
+		</nav-bar>
+		<view class="bg-info" style="height: 1000px;">123</view>
 	</view>
-
 </template>
 
 
 <script>
-	import uniStatusBar from '@/components/uni-ui/uni-status-bar/uni-status-bar.vue'
+	// import uniStatusBar from '@/components/uni-ui/uni-status-bar/uni-status-bar.vue'
+	import navBar from '@/components/common/nav-bar.vue'
 	export default {
 		components: {
-			uniStatusBar
+			// uniStatusBar
+			navBar
 		},
 		data() {
 			return {
@@ -41,6 +32,38 @@
 		},
 		onLoad() {}
 	}
+	
+	/*
+	<template>
+		<view>
+			<!--  自定义导航栏 -->
+			<view>
+				<!-- 驼峰式自动转换为中划线式 -->
+				<!-- 顶部 -->
+				<uni-status-bar>
+					<!-- 此处不为rpx -->
+					<view style="height: 44px;" class="flex border-bottom align-center">
+						<view class="flex-1 flex">
+							<!-- ml  margin left 32 -->
+							<text class="font-md ml-3">首页</text>
+						</view>
+						<view class="flex-1 flex justify-center"></view>
+						<view class="flex-1 flex justify-end">
+							<view style="width: 60rpx;height: 60rpx;" class="flex align-center justify-center bg-light rounded-circle mr-3">
+								<text class="iconfont icon-zengjia"></text>
+							</view>
+							<view style="width: 60rpx;height: 60rpx;" class="flex align-center justify-center bg-light rounded-circle mr-3">
+								<text class="iconfont icon-gengduo"></text>
+							</view>
+						</view>
+					</view>
+				</uni-status-bar>
+			</view> 
+		</view>
+	
+	</template>
+	
+	*/
 </script>
 
 <style>
