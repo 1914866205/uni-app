625e7ec4f6784d660eba61cf622e54523f91cfb9
ntt <1914866205@qq.com>
2020-10-23 14:47:33 +0800

    上传下载

--- network-dist/pages/list/list.vue
+++ network-dist/pages/list/list.vue
@@ -1,7 +1,19 @@
 <template>
-	<view class="content">
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view style="height: 100vh;"class="flex flex-column">
+		<view class="flex border-bottom border-light-secondary"
+		style="height: 100rpx;">
+		<view class="flex-1 flex flex-column align-center justify-center"
+		v-for="(item,index) in tabBars" 
+		:key="index" 
+		:class="index===tabIndex?'text-main':''"
+		@click="changeTab(index)"
+		>
+		<text class="font-md">{{item.name}}</text>
+		<text style="height: 8rpx;width: 140rpx;"
+		class="rounded"
+		:class="tabIndex===index?'bg-main':'bg-white'"
+		></text>
+		</view>
 		</view>
 	</view>
 </template>
@@ -10,6 +22,15 @@
 	export default {
 		data() {
 			return {
+				tabIndex:0,
+				tabBars:[
+					{
+						name:'下载列表'
+					},
+					{
+						name:'上传列表'
+					}
+				],
 				title: 'my'
 			}
 		},
@@ -17,7 +38,9 @@
 
 		},
 		methods: {
-
+			changeTab(index){
+				this.tabIndex=index;
+			}
 		}
 	}
 </script>
