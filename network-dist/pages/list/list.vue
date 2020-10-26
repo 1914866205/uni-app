b9fc3e99f14efaf4b4cf70bdaef97ef221d4e478
ntt <1914866205@qq.com>
2020-10-26 17:04:50 +0800

    下载进度的实现和清除传输记录列表

--- network-dist/pages/list/list.vue
+++ network-dist/pages/list/list.vue
@@ -15,28 +15,28 @@
 			<swiper-item class="flex-1 flex" v-for="(item,index) in tabBars" :key="index">
 				<scroll-view scroll-y="true" class="flex-1">
 					<!-- 下载列表 -->
- 					<template v-if="index===0">
-		<!--				<view style="height: 60rpx;" class="bg-light flex align-center font-sm px-2 text-muted">
+					<template v-if="index===0">
+						<view style="height: 60rpx;" class="bg-light flex align-center font-sm px-2 text-muted">
 							文件下载至: storage/xxxx/xxxx
 						</view>
 						<view class="p-2 border-bottom border-light-secondary font text-muted">
 							下载中{{downing.length}}
-						</view> -->
+						</view>
 						<!-- 因为下面同级还有f-list中绑定了key和index，会冲突，所以要给他们加上不同的后缀区分，否则报错 -->
-					<!-- 	<f-list v-for="(item,index) in downing" :key="'i'+index" :item="item" :index="index">
+						<f-list v-for="(item,index) in downing" :key="'i'+index" :item="item" :index="index">
 							<view style="height: 70rpx;" class="flex align-center text-main">
 								<text class="iconfont icon-zanting"></text>
-								<text class="ml-1">{{item.download}}%</text>
-							</view> -->
+								<text class="ml-1">{{item.progress}}%</text>
+							</view>
 							<!-- 进度条组件,uniapp自带的无需引入，percent属性绑定下载百分比数值 -->
-						<!-- 	<progress slot="bottom" :percent="item.download" activeColor="#009CFF" :stroke-width="4" />
+							<progress slot="bottom" :percent="item.progress" activeColor="#009CFF" :stroke-width="4" />
 						</f-list>
 						<view class="p-2 border-bottom border-light-secondary font text-muted">
 							下载完成({{downed.length}})
 						</view>
 						<f-list v-for="(item,index) in downed" :key="'d'+index" :item="item" :index="index" :showRight="false"></f-list>
-		-->			</template>
- 
+					</template>
+
 					<!-- 上传列表 -->
 
 
@@ -108,7 +108,8 @@
 		},
 		computed: {
 			...mapState({
-				uploadList: state => state.uploadList
+				uploadList: state => state.uploadList,
+				downlist: state => state.downlist
 			}),
 			uploading() {
 				return this.uploadList.filter(item => {
@@ -119,11 +120,35 @@
 				return this.uploadList.filter(item => {
 					return item.progress === 100;
 				})
+			},
+			downing() {
+				return this.downlist.filter(item => {
+					return item.progress < 100;
+				})
+			},
+			downed() {
+				return this.downlist.filter(item => {
+					return item.progress = 100;
+				})
 			}
 		},
 		methods: {
 			changeTab(index) {
 				this.tabIndex = index;
+			},
+			onNavigationBarButtonTab(){
+				uni.showModel({
+					content:'是否要清除传输记录？',
+					success:res=>{
+						if(res.confirm){
+							this.$store.dispatch('clearList');
+							uni.showToast({
+								title:'清除成功',
+								icon:'none'
+							})
+						}
+					}
+				})
 			}
 		}
 	}
