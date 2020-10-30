25d83d8c920250c86b294bafcb6f73128110729b
ntt <1914866205@qq.com>
2020-10-30 14:52:23 +0800

    gift

---
+++ live_stream/common/gift.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    image: '../../static/gift/1.png',
+    id: 1,
+    name: '爱心',
+    coin: 1,
+  },
+  {
+    image: '../../static/gift/2.png',
+    id: 2,
+    name: '蛋糕',
+    coin: 2,
+  },
+  {
+    image: '../../static/gift/3.png',
+    id: 3,
+    name: '巧克力',
+    coin: 3,
+  },
+  {
+    image: '../../static/gift/4.png',
+    id: 4,
+    name: '棒棒糖',
+    coin: 4,
+  },
+  {
+    image: '../../static/gift/5.png',
+    id: 5,
+    name: '红包',
+    coin: 5,
+  },
+]
