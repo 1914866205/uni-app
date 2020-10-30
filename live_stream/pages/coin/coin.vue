1fecb80a3e8ed000bb5390fd9e570e4452632132
ntt <1914866205@qq.com>
2020-10-30 15:16:24 +0800

    chongzhi

---
+++ live_stream/pages/coin/coin.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="border-top border-light-secondary p-3">
+		<view class="rounded py-4 flex flex-column align-center justify-center bg-main">
+
+			<text class="text-white font-sm mb-2">当前金币</text>
+			<text class="font-weight-bold text-white" style="font-size: 60rpx;">50</text>
+		</view>
+		<view class="border-top border-light-secondary my-3"></view>
+		<view>
+			<text class="font-sm text-muted">请选择充值金币</text>
+		</view>
+		<view class="flex flex-wrap" style="margin-left: -20rpx;margin-right: -20rpx;">
+			<view style="width: 33.3%;box-sizing: border-box;" class="p-2" v-for="i in 6">
+				<view style="height: 130rpx;" class="border rounded flex flex-column align-center justify-center">
+					<view style="height: 130rpx;" class="border rounded flex flex-column align-center">
+						<text class="iconfont text-warning mr-1">&#xe633;</text>
+						<text class="font-md font-weight-bold">10</text>
+					</view>
+					<text class="font text-light-muted">￥10</text>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
