531abc40ef0b35091a43145119825241be7c3f34
ntt <1914866205@qq.com>
2020-10-30 22:39:46 +0800

    my

--- live_stream/pages/my/my.vue
+++ live_stream/pages/my/my.vue
@@ -1,22 +1,62 @@
 <template>
 	<view>
-		
+		<view class="top flex align-center justify-center">
+		</view>
+		<view class="flex align-center">
+			<view class="flex align-center justify-center position-relative" style="width: 180rpx;height: 160rpx;">
+				<image src="../../static/ntt.jpg" class="rounded-circle" style="width: 145rpx;height: 145rpx;position: absolute;top: -60rpx;">
+
+				</image>
+			</view>
+			<view class="flex flex-column">
+				<text class="font-md">涛涛</text>
+				<text class="font text-muted">醉忆丶无回路</text>
+			</view>
+
+			<view class="ml-auto mr-3">
+				<view class="border border-main rounded flex align-center justify-center p-2" hover-class="bg-light">
+					<text class="text-main font">编辑资料</text>
+				</view>
+			</view>
+		</view>
+		<view class="f-divider"></view>
+		<f-list-item icon="iconbizhongguanli" title="我的金币" :showRight="false">
+			<text class="text-muted font">50金币 立即充值</text>
+		</f-list-item>
+		<f-list-item icon="iconzhengzaizhibo" title="我的直播">
+			<text class="text-muted font">0</text>
+		</f-list-item>
+		<f-list-item icon="iconfaxian" title="我的关注">
+			<text class="text-muted font">0</text>
+		</f-list-item>
+		<f-list-item icon="iconmore" title="历史记录"></f-list-item>
 	</view>
 </template>
 
 <script>
+	import fListItem from '@/components/live/f-list-item.vue';
 	export default {
+		components: {
+			fListItem
+		},
 		data() {
 			return {
-				
+				statusBarHeight: 0
 			}
 		},
-		methods: {
-			
+		onLoad() {
+			let res = uni.getSystemInfoSync()
+			this.statusBarHeight = res.statusBarHeight
 		}
 	}
 </script>
 
 <style>
-
+	.top {
+		width: 750rpx;
+		height: 260rpx;
+		background-image: url(../../static/06.jpg);
+		background-size: cover;
+		background-image: linear-gradient(to right, #BA7ACE 0%, #8745FF 100%);
+	}
 </style>
